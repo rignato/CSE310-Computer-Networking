8c8a83e02120fc48119c50c40cc90ac3cda78b4e
Robert <robus.i@hotmail.com>
2020-03-14 14:08:00 -0400

    fixed bug in counting packet size

--- hw2/analysis_pcap_tcp.py
+++ hw2/analysis_pcap_tcp.py
@@ -110,7 +110,7 @@ def analyze(pcap, max_pkt_per_flow=2, max_cwnd_rtt_count=5):
                                 rwnd
                             ))
                             src_c += 1
-                        total_data += int(len(tcp.data) + (tcp.off/4))
+                        total_data += int(len(tcp.data) + (tcp.off*4))
                         if tcp.seq+len(tcp.data) in time_dict:
                             if fast_retransmit == tcp.seq:
                                 triple_dup_acks += 1
